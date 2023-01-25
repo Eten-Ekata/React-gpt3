97bc23c430f0e5219795484261b4520bbada3953
Eten-Ekata <ekataelishaeten@gmail.com>
2023-01-24 23:26:05 -0800

    asset folder

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../../assets/logo.svg'
+import{Rimenu3Line, RiCloseLine } from 'react-icons/ri'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links-logo'>
+          <img src={logo} alt="logo"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
